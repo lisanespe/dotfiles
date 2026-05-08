a14321c6eada89ddc8f20d30ac735fa93c8e69d8
lisanespe <lisandroespejo@gmail.com>
2026-05-08 19:26:20 -0300

    [UPD] Updated config files

--- config/waybar/waybar/power-menu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-choice=$(echo -e "Suspend\nHibernate\nShutdown\nReboot" | wofi --dmenu --prompt "Power")
-
-case $choice in
-    "Suspend") systemctl suspend ;;
-    "Hibernate") systemctl hibernate ;;
-    "Shutdown") systemctl poweroff ;;
-    "Reboot") systemctl reboot ;;
-esac
